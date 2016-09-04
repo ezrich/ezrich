3495cf943b794b958af8d0f53dedaf4f27cc796d
ezrich <ezrich@protonmail.com>
2016-09-04 11:05:13 +0200

    Update Currency.h

--- src/CryptoNoteCore/Currency.h
+++ src/CryptoNoteCore/Currency.h
@@ -74,9 +74,8 @@ public:
   const Block& genesisBlock() const { return m_genesisBlock; }
   const Crypto::Hash& genesisBlockHash() const { return m_genesisBlockHash; }
 
-  bool getBlockReward(size_t medianSize, size_t currentBlockSize, uint64_t alreadyGeneratedCoins, uint64_t fee,
-    uint64_t& reward, int64_t& emissionChange) const;
-  size_t maxBlockCumulativeSize(uint64_t height) const;
+  bool getBlockReward(1000000) const;
+  size_t maxBlockCumulativeSize(1000000) const;
 
   bool constructMinerTx(uint32_t height, size_t medianSize, uint64_t alreadyGeneratedCoins, size_t currentBlockSize,
     uint64_t fee, const AccountPublicAddress& minerAddress, Transaction& tx,
